d1fa60159ccad26abcc4923b3d551661b107c923
Hoanggiabao1 <hgiabao88@gmail.com>
2024-03-08 17:59:24 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,48 @@
 #include <SDL_image.h>
 #include <SDL.h>
 
+SDL_Window* cuaSo = NULL;
+SDL_Surface* manHinh = NULL;
+SDL_Surface* banDo = NULL;
+
+const int chieuDai = 1920;
+const int chieuRong = 1080; //(chieu cao)
+
+bool init()
+{
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
+        return false;
+    }
+    else{
+        cuaSo = SDL_CreateWindow( "Pokemon", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, chieuDai, chieuRong, SDL_WINDOW_SHOWN );
+        if( cuaSo == NULL ){
+            return false;
+        }
+        else{
+            manHinh = SDL_GetWindowSurface( cuaSo );
+        }
+    }
+    return true;
+}
+
+void ketThuc()
+{
+    SDL_FreeSurface( banDo );
+    banDo = NULL;
+    SDL_DestroyWindow( cuaSo );
+    cuaSo = NULL;
+    SDL_Quit();
+}
+
 int main(int argc, char* argv[]){
-    
-    if(SDL_Init(SDL_INIT_EVERYTHING) == -1){
+
+    if (!init()){
         return 1;
     }
 
+    SDL_UpdateWindowSurface( cuaSo );
     SDL_Delay(3000);
-
-    SDL_Quit();
+    ketThuc();
 
     return 0;
 }
